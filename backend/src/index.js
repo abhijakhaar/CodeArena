9b9123f61037c7363a52a11eeb0b24c1e143fea7
IshanshK <ishanshkhare.in@gmail.com>
2025-08-12 17:02:24 +0530

    User Schema

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,14 @@
 const express = require("express")
 const app = express()
 const PORT = 3000
-
+app.use(express.json())
 app.get("/", (req,res)=>{
     res.status(201).json({
         "msg": "runs"
     })
 })
+
+// mongoose.connect()
 
 app.listen(PORT, ()=>{
     console.log(`listening on port ${PORT}`)
